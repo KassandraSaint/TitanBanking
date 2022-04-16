b4f849463ecddf495f7b8a7d203a31aee54af8d8
Kassandra Saint <kassandrastripper@gmail.com>
2022-04-15 22:30:48 -0400

    Add server_side node js script

---
+++ app/server.js
@@ -0,0 +1,23 @@
+// Importing http, url and file system modules
+var http = require("http");
+var url = require('url');
+var fileSystem = require('fs');
+
+// Creating http server and passing callback function
+http.createServer(function (request, response) {
+  // Creating url object by parsing url
+  var urlObj = url.parse(request.url, true, false);
+  // Attempting to read a file in the current folder
+  fileSystem.readFile("." + urlObj.pathname, function (error, data) {
+    // Checking if reading a file caused an error
+    if (error) {
+      // If it caused an error returning 404 not found and a message
+      response.writeHead(404);
+      response.end("Sorry, the page you are looking for is not here!");
+      return;
+    }
+    // If there was no error reading a file return 200 OK and show data
+    response.writeHead(200);
+    response.end(data);
+  });
+}).listen(3000);
